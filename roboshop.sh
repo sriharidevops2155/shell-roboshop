1cec72b3f3ba87be3dc56d47b253016370bcea5b
sriharibandi <shreharii@gmail.com>
2025-05-21 21:25:05 +0530

    practise5

--- roboshop.sh
+++ roboshop.sh
@@ -22,7 +22,27 @@ do
         IP=$(aws ec2 describe-instances  --instance-ids $INSTANCE_ID  --query "Reservations[0].Instances[0].PublicIpAddress")
     fi
     echo "$instance IP address is: $IP"
+    
+    aws route53 change-resource-record-sets \
+    --hosted-zone-id $ZONE_ID \
+    --change-batch '
+  {
+    "Comment": "Creating or updating a record set for cognito endpoint"
+    ,"Changes": [{
+      "Action"              : "UPSERT"
+      ,"ResourceRecordSet"  : {
+        "Name"              : "'$instance'.'$DOMAIN_NAME'"
+        ,"Type"             : "A"
+        ,"TTL"              : 1
+        ,"ResourceRecords"  : [{
+            "Value"         : "'$IP'"
+        }]
+      }
+    }]
+  }
 done
+
+
 
 
 
