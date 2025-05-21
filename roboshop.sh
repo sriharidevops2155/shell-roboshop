a17325f1b86435682d71da8384dfa56ec417627e
sriharibandi <shreharii@gmail.com>
2025-05-21 22:54:24 +0530

    practise5

--- roboshop.sh
+++ roboshop.sh
@@ -23,21 +23,4 @@ do
     fi
     echo "$instance IP address is: $IP"
     
-    aws route53 change-resource-record-sets \
-    --hosted-zone-id $ZONE_ID \
-    --change-batch '
-    {
-        "Comment": "Creating or Updating a record set for cognito endpoint"
-        ,"Changes": [{
-        "Action"              : "UPSERT"
-        ,"ResourceRecordSet"  : {
-            "Name"              : "'$instance'.'$DOMAIN_NAME"
-            ,"Type"             : "A"
-            ,"TTL"              : 1
-            ,"ResourceRecords"  : [{
-                "Value"         : "'$IP"
-            }]
-        }
-        }]
-    }'
 done
