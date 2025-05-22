51b4b5d7e6ceb8adf11df2698d118edd4be686c2
sriharibandi <shreharii@gmail.com>
2025-05-22 18:39:42 +0530

    practise5

--- roboshop.sh
+++ roboshop.sh
@@ -17,9 +17,11 @@ do
     --output text)
     if [ $instance != "frontend" ]
     then 
-        IP=$(aws ec2 describe-instances  --instance-ids $INSTANCE_ID  --query "Reservations[0].Instances[0].PrivateIpAddress"  --output text) 
+        IP=$(aws ec2 describe-instances  --instance-ids $INSTANCE_ID  --query "Reservations[0].Instances[0].PrivateIpAddress"  --output text)
+        RECORD_NAME="$instance.$DOMAIN_NAME"
     else
         IP=$(aws ec2 describe-instances  --instance-ids $INSTANCE_ID  --query "Reservations[0].Instances[0].PublicIpAddress" --output text)
+        RECORD_NAME="$DOMAIN_NAME"
     fi
     echo "$instance IP address is: $IP"
     aws route53 change-resource-record-sets \
@@ -30,7 +32,7 @@ do
     ,"Changes": [{
     "Action"              : "UPSERT"
     ,"ResourceRecordSet"  : {
-        "Name"              : "'$instance'.'$DOMAIN_NAME'"
+        "Name"              : "'$RECORD_NAME'"
         ,"Type"             : "A"
         ,"TTL"              : 1
         ,"ResourceRecords"  : [{
