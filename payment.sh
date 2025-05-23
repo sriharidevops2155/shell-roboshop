d4f7aa14dd4539bbefbde68984388b95e1cd702c
sriharibandi <shreharii@gmail.com>
2025-05-23 23:40:40 +0530

    practise5

--- payment.sh
+++ payment.sh
@@ -53,6 +53,7 @@ VALIDATE $? "Creating app directory"
 curl -L -o /tmp/payment.zip https://roboshop-artifacts.s3.amazonaws.com/payment-v3.zip  &>>$LOG_FILE
 VALIDATE $? "Downloading the payments app"
 
+cd /app 
 rm -rf /app/*
 unzip /tmp/payment.zip &>>$LOG_FILE
 VALIDATE $? "unzipping payments"
