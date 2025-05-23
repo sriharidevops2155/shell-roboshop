bdefd16e38c1683a22875fbf22cced53ca5915e1
sriharibandi <shreharii@gmail.com>
2025-05-23 19:53:47 +0530

    practise5

--- shipping.sh
+++ shipping.sh
@@ -77,11 +77,19 @@ VALIDATE $? "staring shipping service"
 
 dnf install mysql -y &>>$LOG_FILE
 VALIDATE $? "Installing my sql"
+ 
+mysql -h mysql.daws84s.cloud -u root -pRoboShop@1 -e 'use cities'
 
-mysql -h mysql.daws84s.cloud -uroot -pRoboShop@1 < /app/db/schema.sql &>>$LOG_FILE
-mysql -h mysql.daws84s.cloud -uroot -pRoboShop@1 < /app/db/app-user.sql &>>$LOG_FILE
-mysql -h mysql.daws84s.cloud -uroot -pRoboShop@1 < /app/db/master-data.sql &>>$LOG_FILE
-VALIDATE $? "Loading MySQL data"
+if [ $? -ne 0 ]
+then
+    mysql -h mysql.daws84s.cloud -uroot -pRoboShop@1 < /app/db/schema.sql &>>$LOG_FILE
+    mysql -h mysql.daws84s.cloud -uroot -pRoboShop@1 < /app/db/app-user.sql &>>$LOG_FILE
+    mysql -h mysql.daws84s.cloud -uroot -pRoboShop@1 < /app/db/master-data.sql &>>$LOG_FILE
+    VALIDATE $? "Loading MySQL data"
+else
+    echo -e "Data is loaded already loaded into MYSQL... $Y SKIPPING $N"
+
+fi
 
 systemctl restart shipping &>>$LOG_FILE
 VALIDATE $? "restart shipping"
