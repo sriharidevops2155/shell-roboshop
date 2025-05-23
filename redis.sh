25f5a24a6fcdd204e9c408e119882769fbaca734
sriharibandi <shreharii@gmail.com>
2025-05-23 13:26:56 +0530

    practise5

--- redis.sh
+++ redis.sh
@@ -12,10 +12,8 @@ LOGS_FOLDER="/var/log/redis-logs"
 SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
 LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log"
 
-
 mkdir -p $LOGS_FOLDER
 echo "Script started executing at: $(date)" | tee -a $LOG_FILE 
-
 
 #check the user have root privilages or not ? 
 if [ $USERID -ne 0 ]
@@ -38,22 +36,25 @@ VALIDATE()
     fi
 }
 
-
-dnf module disable redis -y
+dnf module disable redis -y  &>>$LOG_FILE
 VALIDATE $? "Disabling redis"
 
-dnf module enable redis:7 -y
+dnf module enable redis:7 -y &>>$LOG_FILE
 VALIDATE $? "Enabling redis:7"
 
-dnf install redis -y
+dnf install redis -y &>>$LOG_FILE
 VALIDATE $? "Installing redis:7"
 
-sed -i "s/127.0.0.1/0.0.0.0/" /etc/redis/redis.conf
-VALIDATE $? "Updated IP address in redis.conf"
+sed -i -e "s/127.0.0.1/0.0.0.0/g" -e "/protected-mode/ c protected-mode no" /etc/redis/redis.conf
+VALIDATE $? "Edited redis.conf to accept remote connections"
 
-
-
-systemctl enable redis 
+systemctl enable redis &>>$LOG_FILE
 VALIDATE $? "Enabling redis:7"
-systemctl start redis 
+
+systemctl start redis &>>$LOG_FILE
 VALIDATE $? "Starting redis:7"
+
+END_TIME=$(date +%s)
+TOTAL_TIME=$(($END_TIME - $START_TIME))
+
+echo -e "Script execution completed sucessfully, $Y time taken:  $TOTAL_TIME $N" &>>$LOG_FILE
