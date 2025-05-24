65ccae182947c887f9626f1893c183c37699061c
sriharibandi <shreharii@gmail.com>
2025-05-24 15:37:57 +0530

    practise5

--- catalogue.sh
+++ catalogue.sh
@@ -1,88 +1,12 @@
 #!/bin/bash
 
-USERID=$(id -u)
+source ./common.sh
+app_name=catalogue
 
-R="\e[31m"
-G="\e[32m"
-Y="\e[33m"
-N="\e[0m"
-LOGS_FOLDER="/var/log/catalogue-logs"
-SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
-LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log"
-SCRIPT_DIR=$PWD
-
-
-mkdir -p $LOGS_FOLDER
-echo "Script started executing at: $(date)" | tee -a $LOG_FILE
-
-#check the user have root privilages or not ? 
-if [ $USERID -ne 0 ]
-then 
-    echo -e "$R ERROR: Please run the user with root access $N" | tee -a $LOG_FILE  
-    exit 1 #give other than 0 upto 127 
-else 
-    echo "You are running with root access" | tee -a $LOG_FILE 
-fi
-
-#validating if installation is sucedded or not 
-VALIDATE()
-{
-    if [ $1 -eq 0 ]
-    then
-       echo -e "$2 is ...$G SUCESS $N" | tee -a $LOG_FILE 
-    else
-       echo -e " $2 is ...$R FAILURE $N "| tee -a $LOG_FILE 
-        exit 1
-    fi
-}
-
-dnf module disable nodejs -y &>>$LOG_FILE
-VALIDATE $? "Disabling default node js"
-
-dnf module enable nodejs:20 -y &>>$LOG_FILE
-VALIDATE $? "Enabling nodejs 20"
-
-dnf install nodejs -y &>>$LOG_FILE
-VALIDATE $? "Installing nodejs:20"
-
-id roboshop &>>$LOG_FILE
-
-if [ $? -ne 0 ]
-then
-    useradd --system --home /app --shell /sbin/nologin --comment "roboshop system user" roboshop &>>$LOG_FILE
-    VALIDATE $? "Creating system user"
-else
-    echo -e "System user roboshop is alread created... $Y skipping $N"
-fi
-
-mkdir -p /app   
-VALIDATE $? "Creating app directory"
-
-curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue-v3.zip &>>$LOG_FILE
-VALIDATE $? "Downloading the catalogue app"
-
-rm -rf /app/*
-VALIDATE $? "removing the existing content"
-cd /app 
-VALIDATE $? "Moving to app directory"
-
-unzip /tmp/catalogue.zip &>>$LOG_FILE
-VALIDATE $? "unzipping catalogue"
-
-npm install  &>>$LOG_FILE
-VALIDATE $? "installing Dependencies"
-
-cp $SCRIPT_DIR/catalogue.service /etc/systemd/system/catalogue.service
-VALIDATE $? "Copying catalogue service"
-
-systemctl daemon-reload &>>$LOG_FILE
-VALIDATE $? "deamon-reload for catalogue service"
-
-systemctl enable catalogue &>>$LOG_FILE
-VALIDATE $? "enabling catalogue service"
-
-systemctl start catalogue 
-VALIDATE $? "staring catalogue service"
+check_root
+app_setup
+nodejs_setup
+systemd_setup
 
 cp $SCRIPT_DIR/mongo.repo /etc/yum.repos.d/mongo.repo
 dnf install mongodb-mongosh -y &>>$LOG_FILE
@@ -97,6 +21,7 @@ else
     echo -e "Data is already loaded.......... $Y Skipping $N"
 fi
 
+print_time
 
 
 
