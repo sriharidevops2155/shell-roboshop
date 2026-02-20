5dad6de1887e1b8be796721c6b36894cf9b6be86
sriharibandi <shreharii@gmail.com>
2026-02-20 20:44:41 +0530

    shell

--- rabbitmq.sh
+++ rabbitmq.sh
@@ -23,6 +23,9 @@ else
     echo "You are running with root access" | tee -a $LOG_FILE 
 fi
 
+echo "Please enter rabbitmq password to setup"
+read -s RABBITMQ_PASSWD
+
 #validating if installation is sucedded or not 
 VALIDATE()
 {
@@ -47,7 +50,7 @@ VALIDATE $? "Enabling rabbitmq-server"
 systemctl start rabbitmq-server  &>>$LOG_FILE
 VALIDATE $? "Starting  rabbitmq-server"
 
-rabbitmqctl add_user roboshop roboshop123  &>>$LOG_FILE
+rabbitmqctl add_user roboshop $RABBITMQ_PASSWD  &>>$LOG_FILE
 VALIDATE $? "Starting  rabbitmq-server"
 
 rabbitmqctl set_permissions -p / roboshop ".*" ".*" ".*"  &>>$LOG_FILE
